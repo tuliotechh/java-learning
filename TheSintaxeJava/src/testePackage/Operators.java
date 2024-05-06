ff92ca5672d904b94dad67b2b4907fa7c58ec798
Marco Tulio <110563678+tuliotechh@users.noreply.github.com>
2024-05-06 15:55:59 -0300

    class operators

---
+++ TheSintaxeJava/src/testePackage/Operators.java
@@ -0,0 +1,39 @@
+package testePackage;
+
+public class Operators {
+    public static void main (String [] args){
+        System.out.println("Hello World");
+
+//      Operadores de Atribuição (= ou ==)
+//      Operadores de Aritimeticos (=, -, *, /)
+
+        double soma = 10.5 + 15.7;
+        int subtração = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18 % 3;
+        double resultado = (10 * 7) + (20/4);
+
+        //classe Operadores.java
+        int numero = 5;
+
+//      Imprimindo o numero negativo
+        System.out.println(- numero);
+
+//      incrementando numero em mais 1 numero, imprime 6
+        numero ++;
+        System.out.println(numero);
+
+//      incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero ++);// ops algo de errado não está certo
+
+        System.out.println(numero);// agora sim, numero virou 7
+
+//      ordem de precedencia conta aqui
+        System.out.println(++ numero);
+
+        boolean verdadeiro = true;
+
+        System.out.println("Inverteu " + !verdadeiro);
+    }
+}
