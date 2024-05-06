da667d2db9b4596e05e8b9edde58eb61762721e8
Marco Tulio <110563678+tuliotechh@users.noreply.github.com>
2024-05-06 13:51:14 -0300

    Create VariablesTipes.java

---
+++ TheSintaxeJava/src/testePackage/VariablesTipes.java
@@ -0,0 +1,20 @@
+package testePackage;
+
+public class VariablesTipes {
+    public static void main (String [] args) {
+//        System.out.printf("Hello World");
+//        byte idade = 123;
+//        short ano = 2024;
+//        int cep = 38080500;
+//        long cpf = 1467542895L;
+//        float pi = 3.14F;
+//        double salario = 2500;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        final int NUMERO = 5; // Constante (final) impossibilita q outro valor seja atribuido na variavel
+        NUMERO = 10;
+    }
+}
