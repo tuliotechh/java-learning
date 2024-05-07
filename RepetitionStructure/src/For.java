058389b7d7f6a309b8c45733e0565a8245938373
Marco Tulio <110563678+tuliotechh@users.noreply.github.com>
2024-05-07 20:42:13 -0300

    material repeating structures

---
+++ RepetitionStructure/src/For.java
@@ -0,0 +1,21 @@
+public class For {
+    public static void main(String[] args) {
+        // For = faça
+
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++){
+            System.out.println(carneirinhos +" Carneirinho");
+        }
+        System.out.println("Joao adormeceu...");
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
